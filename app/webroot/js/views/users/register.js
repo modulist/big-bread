cbb975c99bf7a6bcb94be4d9e3e99b5819d73f5c
Rob Wilkerson <rob@robwilkerson.org>
2011-04-12 16:29:43 -0400

    Fixing an icon selection bug.

--- app/webroot/js/views/users/register.js
+++ app/webroot/js/views/users/register.js
@@ -2,9 +2,8 @@ $(document).ready( function() {
   $('#user_type img')
     .css( 'cursor', 'pointer' )
     .click( function() {
-      id = $(this).attr( 'data-for' );
-      // TODO: "-a" in the UUID is being converted to "A" and messing stuff up
-      $('#' + id).attr( 'checked', 'checked' );
+      // when the icon is clicked, select the appropriate radio option
+      $(this).nextAll( 'input[type="radio"]' ).attr( 'checked', 'checked' );
     });
     
   $('input[type="reset"]').click( function() {
