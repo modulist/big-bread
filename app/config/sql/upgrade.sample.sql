24b33640ada6aa24fc6cab8dae80c3563830abbd
Rob Wilkerson <rob@robwilkerson.org>
2011-04-13 20:48:11 -0400

    Upgrade to prevent the wall system error.

--- app/config/sql/upgrade.sample.sql
+++ app/config/sql/upgrade.sample.sql
@@ -1,3 +1,25 @@
 USE @DB_NAME@;
 
--- EMPTY
+SET foreign_key_checks = 0;
+
+ALTER TABLE building_wall_systems
+  DISABLE KEYS,
+  DROP FOREIGN KEY fk__building_wall_systems__buildings,
+  DROP FOREIGN KEY fk__building_wall_systems__wall_systems,
+  DROP KEY fk__building_wall_systems__wall_systems,
+  DROP PRIMARY KEY,
+  MODIFY COLUMN wall_system_id char(36) NULL,
+  ADD PRIMARY KEY( id );
+
+ALTER TABLE building_wall_systems
+  ADD CONSTRAINT fk__building_wall_systems__buildings FOREIGN KEY( building_id )
+    REFERENCES buildings( id )
+      ON UPDATE CASCADE
+      ON DELETE NO ACTION,
+  ADD CONSTRAINT fk__building_wall_systems__wall_systems FOREIGN KEY( wall_system_id )
+    REFERENCES wall_systems( id )
+      ON UPDATE CASCADE
+      ON DELETE SET NULL,
+  ENABLE KEYS;
+
+SET foreign_key_checks = 1;
