f7b4ed4ad1dd786d7c22791b1a0d032b8cd97f6b
Rob Wilkerson <rob@robwilkerson.org>
2011-05-26 16:35:23 -0400

    Ensuring that the entered zip is in the database.

--- app/models/address.php
+++ app/models/address.php
@@ -23,7 +23,32 @@ class Address extends AppModel {
 				'message'    => 'Zip code must be a valid US postal code.',
 				'allowEmpty' => false,
 				'required'   => true,
+        'last'       => true,
 			),
+      'known' => array(
+        'rule'    => array( 'known' ), 
+        'message' => 'Please check the zip code. This one is not in our database.' 
+      ),
 		),
 	);
+  
+  /**
+   * Custom validation method to ensure that the entered zip exists in
+   * the database. Everything we do hinges on the zip code so an invalid
+   * value needs to be caught and reported.
+   *
+   * @param   $field
+   * @access  public
+   */
+  public function known( $field ) {
+    $zip = array_shift( $field );
+    
+    return $this->ZipCode->find(
+      'count',
+      array(
+        'contain'    => false,
+        'conditions' => array( 'ZipCode.zip' => $zip ),
+      )
+    );
+  }
 }
