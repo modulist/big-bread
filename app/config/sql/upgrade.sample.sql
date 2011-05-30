9fe289b4c734dbcec9787d636cb8b6b0e188fd43
Rob Wilkerson <rob@robwilkerson.org>
2011-05-30 13:26:36 -0400

    Removing released content from upgrade.sql.

--- app/config/sql/upgrade.sample.sql
+++ app/config/sql/upgrade.sample.sql
@@ -2,43 +2,6 @@ USE @DB_NAME@;
 
 SET foreign_key_checks = 0;
 
--- Support proposal requests
-DROP TABLE IF EXISTS proposals;
-CREATE TABLE proposals(
-  id            char(36)      NOT NULL,
-  user_id       char(36)      NOT NULL, -- sender
-  incentive_id  char(36)      NOT NULL, -- the incentive from which the request was generated
-  technology_id char(36)      NOT NULL, -- the technology from which the request was generated
-  scope_of_work varchar(255)  NOT NULL, -- install | repair
-  timing        varchar(255)  NOT NULL, -- ready | planning
-  urgency       varchar(255)  NOT NULL, -- within a week | 1-2 weeks | over 3 weeks | flexible
-  comments      text          NULL,
-  created       datetime      NOT NULL,
-  modified      datetime      NOT NULL,
-  
-  PRIMARY KEY( id ),
-  CONSTRAINT  fk__proposals__users FOREIGN KEY( user_id )
-    REFERENCES users( id )
-    ON UPDATE CASCADE
-    ON DELETE NO ACTION,
-  CONSTRAINT  fk__proposals__incentive FOREIGN KEY( incentive_id )
-    REFERENCES incentive( id )
-    ON UPDATE CASCADE
-    ON DELETE NO ACTION,
-  CONSTRAINT  fk__proposals__technologies FOREIGN KEY( technology_id )
-    REFERENCES technologies( id )
-    ON UPDATE CASCADE
-    ON DELETE NO ACTION
-) ENGINE=InnoDB;
-
--- Admin functionality
-ALTER TABLE users
-  ADD admin boolean NOT NULL DEFAULT 0 AFTER password;
-  
-UPDATE users
-   SET admin = 1
- WHERE email = 'wamaull@mac.com';
- 
 -- Changes to support API usage
 INSERT INTO user_types( id, code, name, deleted )
 VALUES( UUID(), 'API', 'API Consumer', 0 );
@@ -77,17 +40,7 @@ CREATE TABLE api_users(
     ON UPDATE CASCADE
     ON DELETE CASCADE
 ) ENGINE=InnoDB;
-
--- Changes required by the verification tool
-ALTER TABLE incentive_weblink_verification
-  MODIFY incentive_id char(36) NOT NULL,
-  MODIFY source_id bigint NOT NULL,
-  ADD ekg varchar(255) NULL,
-  ADD CONSTRAINT fk__incentive_weblink_verification__incentive FOREIGN KEY( incentive_id )
-    REFERENCES incentive(id)
-    ON UPDATE CASCADE
-    ON DELETE CASCADE;
-    
+  
 -- Original tables no longer necessary
 DROP TABLE partner_domains;
 DROP TABLE partners;
