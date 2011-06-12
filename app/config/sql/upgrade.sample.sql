4e21c0fb7c3d9a906b723605d19460d558539105
Rob Wilkerson <rob@robwilkerson.org>
2011-06-12 17:56:19 -0400

    Removing SQL that was released with v1.5.0

--- app/config/sql/upgrade.sample.sql
+++ app/config/sql/upgrade.sample.sql
@@ -2,6 +2,11 @@ USE @DB_NAME@;
 
 SET foreign_key_checks = 0;
 
+-- Changes to support Contractor users
+INSERT INTO user_types( id, code, name, selectable, deleted ) VALUES
+  ( '6573bca8-945a-11e0-adec-3aadb68782f6', 'CNTRCT', 'Contractor', 1, 0 )
+;
+
 DROP TABLE IF EXISTS contractors;
 CREATE TABLE contractors(
   id                  char(36)  NOT NULL,
