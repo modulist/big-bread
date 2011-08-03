3ce3fe59109b7fbe7f202749a5ed1733872c4ee4
Claudio Luis Vera <modulist@gmail.com>
2011-08-03 10:13:36 -0400

    adding a modified deply script for Traktek

--- deploy.traktek.sh
+++ deploy.traktek.sh
@@ -3,6 +3,7 @@
 # project_name=bigbread
 # build_root=~/Development/.build
 env="dev"
+host="bigbread.traktekpartners.com"
 root="/var/www/sites/clients/bigbread"
 
 if [ $# -lt 1 ]; then
@@ -13,7 +14,7 @@ fi
 dbpassword=$1
 
 echo "Updating the bigbread database from production..."
-ssh  "mysqldump -hbigbread.net -ubigbread -p'$1' --opt --complete-insert bigbread_prd | sed -e s/bigbread_prd/bigbread/ | mysql -udrupal -psql5Serv"
+ssh root@$host "mysqldump -hbigbread.net -ubigbread -p'$1' --opt --complete-insert bigbread_prd | sed -e s/bigbread_prd/bigbread/ | mysql -udrupal -psql5Serv bigbread"
 echo "...complete."
 
 echo "Sync'ing updated files to $root..."
@@ -34,31 +35,33 @@ rsync -vcrlDtOzi --progress \
       --exclude "app/webroot/.htaccess*" \
       --exclude "app/webroot/robots.*.txt" \
       --exclude "deploy.*.sh" \
+      --exclude "_notes" \
+      --exclude "dwsync.xml" \
       --links \
       --delete \
-      ./ bigbread:$root
+      ./ root@$host:$root
 
 echo "Uploading environment-specific files..."
 if [ -f "app/config/core.$env.php" ]; then
-  scp app/config/core.$env.php bigbread:$root/app/config/core.php
+  scp app/config/core.$env.php root@$host:$root/app/config/core.php
 else
-  scp app/config/core.sample.php bigbread:$root/app/config/core.php
+  scp app/config/core.sample.php root@$host:$root/app/config/core.php
 fi
-scp app/config/bootstrap.$env.php bigbread:$root/app/config/bootstrap.local.php
-scp app/config/database.$env.php bigbread:$root/app/config/database.php
-scp app/webroot/.htaccess.$env bigbread:$root/app/webroot/.htaccess
-scp app/webroot/robots.$env.txt bigbread:$root/app/webroot/robots.txt
+scp app/config/bootstrap.$env.php root@$host:$root/app/config/bootstrap.local.php
+scp app/config/database.$env.php root@$host:$root/app/config/database.php
+scp app/webroot/.htaccess.$env root@$host:$root/app/webroot/.htaccess
+scp app/webroot/robots.$env.txt root@$host:$root/app/webroot/robots.txt
 
 echo "...complete."
 
 echo "Clearing cache..."
-ssh bigbread "find $root/app/tmp/cache -type f -exec rm {} \;"
+ssh root@$host "find $root/app/tmp/cache -type f -exec rm {} \;"
 echo "...complete."
 
 # Execute the upgrade.sql file
 echo "Running upgrade.sql..."
-ssh bigbread "cat $root/app/config/sql/upgrade.sample.sql | sed -e s/@DB_NAME@/bigbread/ > $root/app/config/sql/upgrade.sql"
-ssh bigbread "cat $root/app/config/sql/upgrade.sql | mysql -udrupal -psql5Serv"
+ssh root@$host "cat $root/app/config/sql/upgrade.sample.sql | sed -e s/@DB_NAME@/bigbread/ > $root/app/config/sql/upgrade.sql"
+ssh root@$host "cat $root/app/config/sql/upgrade.sql | mysql -udrupal -psql5Serv"
 echo "...complete."
 echo ""
 echo "That's it. Everything should be ready to go"
