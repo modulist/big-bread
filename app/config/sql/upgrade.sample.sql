3ef7db9cf5de4c1fdc9b186638f4dfb4de54e0a4
Rob Wilkerson <rob@robwilkerson.org>
2011-08-14 12:26:36 -0400

    Fixing SQL script issues.

--- app/config/sql/upgrade.sample.sql
+++ app/config/sql/upgrade.sample.sql
@@ -6,17 +6,17 @@ SET foreign_key_checks = 0;
 DROP TABLE IF EXISTS watched_technologies;
 CREATE TABLE watched_technologies(
   id            char(36)    NOT NULL,
-  user_id       char(36)    NOT NULL,
-  technology_id char(36)    NOT NULL,
-  created     datetime      NULL,
-  modified    datetime      NULL,
+  user_id       char(36)    COLLATE utf8_unicode_ci NOT NULL,
+  technology_id char(36)    COLLATE utf8_unicode_ci NOT NULL,
+  created       datetime    NULL,
+  modified      datetime    NULL,
   
-  PRIMARY KEY( id )
+  PRIMARY KEY( id ),
   CONSTRAINT fk__watched_tech__users FOREIGN KEY( user_id )
     REFERENCES users( id )
     ON UPDATE CASCADE
     ON DELETE CASCADE,
-  CONSTRAINT fk__watched_tech__technologies FOREIGN KEY( technologies )
+  CONSTRAINT fk__watched_tech__technologies FOREIGN KEY( technology_id )
     REFERENCES technologies( id )
     ON UPDATE CASCADE
     ON DELETE CASCADE
