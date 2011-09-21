96fce45d0f6010588018381313bd2d68d0861282
Rob Wilkerson <rob@robwilkerson.org>
2011-09-21 18:28:54 -0400

    Reassigning kitchen and laundry appliances.

--- app/config/sql/upgrade.sample.sql
+++ app/config/sql/upgrade.sample.sql
@@ -10,7 +10,7 @@ CREATE TABLE watched_technologies(
   technology_id char(36)    COLLATE utf8_unicode_ci NOT NULL,
   created       datetime    NULL,
   modified      datetime    NULL,
-  
+
   PRIMARY KEY( id ),
   CONSTRAINT fk__watched_tech__users FOREIGN KEY( user_id )
     REFERENCES users( id )
@@ -32,5 +32,28 @@ ALTER TABLE users
 
 ALTER TABLE buildings
   ADD name varchar(255) NULL AFTER id;
+
+ALTER TABLE technologies
+  ADD CONSTRAINT fk__technologies__technology_groups FOREIGN KEY( technology_group_id )
+    REFERENCES technology_groups( id )
+    ON DELETE NO ACTION
+    ON UPDATE CASCADE;
+
+INSERT INTO technology_groups( id, incentive_tech_group_id, name, parent_group_id, rebate_bar )
+VALUES
+  ( '4e7a5b45-387c-4ef6-81bb-22536e891b5e', 'KTCH', 'Kitchen', null, 1 ),
+  ( '4e7a5b45-6054-4f0c-b939-22536e891b5e', 'LNDR', 'Laundry', null, 1 );
+
+UPDATE technology_groups
+   SET rebate_bar = 0
+ WHERE incentive_tech_group_id = 'APP';
+
+UPDATE technologies
+   SET technology_group_id = '4e7a5b45-387c-4ef6-81bb-22536e891b5e' -- Kitchen
+ WHERE incentive_tech_id IN ( 'COOK', 'DISHW', 'FREEZ', 'RFRG' );
+
+UPDATE technologies
+   SET technology_group_id = '4e7a5b45-6054-4f0c-b939-22536e891b5e' -- Laundry
+ WHERE incentive_tech_id IN ( 'WASH', 'DRYER' );
 
 SET foreign_key_checks = 1;
